44dd85d1d3d91d6d42239af8f62c2ea6d5f1dcd6
Tran Thi Thu Trang <42.tranthithutrang.12a14@gmail.com>
2026-07-08 14:41:30 +0700

    Change accent color from terracotta to elegant navy blue

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -12,13 +12,13 @@ export default function Icon() {
           height: "100%",
           display: "flex",
           flexWrap: "wrap",
-          background: "#b5651d",
+          background: "#1e4c74",
           borderRadius: 6,
         }}
       >
         <div style={{ width: "50%", height: "50%", background: "#faf8f4", opacity: 0.9 }} />
-        <div style={{ width: "50%", height: "50%", background: "#b5651d" }} />
-        <div style={{ width: "50%", height: "50%", background: "#b5651d" }} />
+        <div style={{ width: "50%", height: "50%", background: "#1e4c74" }} />
+        <div style={{ width: "50%", height: "50%", background: "#1e4c74" }} />
         <div style={{ width: "50%", height: "50%", background: "#faf8f4", opacity: 0.9 }} />
       </div>
     ),
